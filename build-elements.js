0b8f1e27645bcc1b9c125b3ba10bb7451a4f1930
Anand <anandchakru@gmail.com>
2019-06-25 23:01:54 -0700

    no contact dist/elements*.js files

--- build-elements.js
+++ build-elements.js
@@ -1,36 +1,22 @@
 const fs = require('fs-extra');
-const concat = require('concat');
 
 (async function build() {
 
-  const files_es5 = [
-    './dist/sdk/runtime-es5.js',
-    './dist/sdk/polyfills-es5.js',
-    './dist/sdk/main-es5.js'
-  ];
-
-  const files_es2015 = [
-    './dist/sdk/runtime-es2015.js',
-    './dist/sdk/polyfills-es2015.js',
-    './dist/sdk/main-es2015.js'
-  ];
+  await fs.remove('./dist/elements');
   await fs.ensureDir('./dist/sdk');
   await fs.ensureDir('./dist/elements');
-  await fs.ensureDir('./dist/elements/test');
-
-  await concat(files_es5, 'dist/sdk/nivite-sdk-es5.js');
-  await concat(files_es2015, 'dist/sdk/nivite-sdk-es2015.js');
-
-  // For testing in local
-  fs.copy('dist/sdk/nivite-sdk-es5.js', 'src/assets/nivite-sdk-es5.js', { overwrite: true });
-  fs.copy('dist/sdk/nivite-sdk-es2015.js', 'src/assets/nivite-sdk-es2015.js', { overwrite: true });
-  fs.copy('dist/sdk/styles.css', 'src/assets/styles.css', { overwrite: true });
 
   // For publishing to npm
-  fs.copy('dist/sdk/nivite-sdk-es5.js', 'dist/elements/nivite-sdk-es5.js', { overwrite: true });
-  fs.copy('dist/sdk/nivite-sdk-es2015.js', 'dist/elements/nivite-sdk-es2015.js', { overwrite: true });
-  fs.copy('dist/sdk/styles.css', 'dist/elements/styles.css', { overwrite: true });
-  fs.copy('src/styles.scss', 'dist/elements/scss/styles.scss', { overwrite: true });
-  fs.copy('sample.json', 'dist/elements/test/sample.json', { overwrite: true });
+  await fs.copySync('./dist/sdk', './dist/elements', {
+    overwrite: true, filter: (src, dst) => {
+      const matches = src.match(/(.dist\/sdk)|(dist\/sdk\/.*(css|js))/);  // https://regex101.com/r/k1jI5l/1
+      return matches && matches.length;
+    }
+  });
+  await fs.copy('./src/styles.scss', './dist/elements/styles.scss', { overwrite: true });
+  await fs.copy('./src/sample.json', './dist/elements/sample.json', { overwrite: true });
+
+  await fs.remove('./dist/elements/assets');
+  await fs.remove('./dist/elements/index.html');
 
 })()
